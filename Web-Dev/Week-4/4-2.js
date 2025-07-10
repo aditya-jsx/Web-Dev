3c5981e0a2393a312310fb91f651b1e515e8ecec
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-07-11 00:45:48 +0530

    Start of Todo App Backend

--- Web-Dev/Week-4/4-2.js
+++ Web-Dev/Week-4/4-2.js
@@ -1,6 +1,55 @@
+// Notes :- https://petal-estimate-4e9.notion.site/Intro-to-HTTP-26c5803f153b4401aa76e9fac08ac427
+
+
+
+
+
 //! every domain name has a code which points to its location on the web
 
 //? every server can have multiple operations running on it, different port numbers are used to target different operations running on the server, every domain address has a port number in the end which is used for targeting a specific operation
 
 
 //! A Single server can run multiple projects
+
+
+
+
+
+
+
+
+
+
+
+
+//! Methods: - helps us to specify the type of action that the client wants to perform on a resource on a server.
+
+// CRUD stands for - create, read, update and delete
+
+// any request that we send to the server for getting the website is a get request
+// the very first request that we send to a server for any domain is a get request to get the html for the wesite
+
+
+
+
+
+//! Response - responses could be
+
+// server also gives us a status code, which tells us how everything went
+// there are different series of code that tells us different things
+// bad companies don't status codes, 
+// error should be specified in the status code not the body
+
+
+
+
+//! Body or payload is the part of the HTTP message that contains the actual message that is being sent to the server
+
+
+
+
+
+
+
+//! Routes helps us in the things that we want to do, and on what server we want to do that to.
+// it helps us to do something specific on a specific server.
