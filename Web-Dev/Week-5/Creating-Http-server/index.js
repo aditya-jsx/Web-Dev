c9c8e8f0166fc40d65046f33ecc1beb520c42d9b
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-07-25 03:15:39 +0530

    completed week 5 offline 5.1

---
+++ Web-Dev/Week-5/Creating-Http-server/index.js
@@ -0,0 +1,150 @@
+//! Create an HTTP Server
+
+//! It should have 4 routes
+
+//! 1. http://localhost:3000/multiply?a=1&b=2
+//! 2. http://localhost:3000/add?a=1&b=2
+//! 3. http://localhost:3000/divide?a=1&b=2
+//! 4. http://localhost:3000/subtract?a=1&b=2
+
+//! Inputs given at the end after `?` are known as query parameters (usually used in GET requests)
+
+//! The way to get them in an HTTP route is by extracting them from the `req` argument (req.query.a , req.query.b)
+
+
+
+
+
+// const express = require("express");
+
+// const app = express();
+
+
+// function sum(a, b){
+//     const sum = a + b;
+//     return sum;
+// }
+
+// function multiply(a, b){
+//     const product = a*b;
+//     return product;
+// }
+
+// function divide(a, b){  
+//     const quotient = a/b;
+//     return quotient;
+// }
+
+// function subtract(a, b){
+//     const ans = a - b;
+//     return ans;
+// }
+
+
+// app.get("/sum", function(req, res) {
+//     const a = parseInt(req.query.a);
+//     const b = parseInt(req.query.b);
+
+//     res.json((sum(a, b)))
+// });
+
+// app.get("/multiply", function(req, res) {
+//     const a = parseInt(req.query.a);
+//     const b = parseInt(req.query.b);
+
+//     res.json(multiply(a, b))
+// });
+
+// app.get("/divide", function(req, res) {
+//     const a = parseInt(req.query.a);
+//     const b = parseInt(req.query.b);
+
+//     res.json(divide(a, b))
+// });
+
+// app.get("/subtract", function(req, res) {
+//     const a = parseInt(req.query.a);
+//     const b = parseInt(req.query.b);
+
+//     res.json(subtract(a, b))
+// });
+
+// app.listen(3000);
+
+
+
+//! Working
+
+
+
+
+//! now the next task is how can we make the users give value like this
+//! http://localhost:3000/multiply/1/2
+
+
+
+//? we can do this by passing routes like this "/sum/:a/:b"
+
+
+const express = require("express");
+
+const app = express();
+
+
+function sum(a, b){
+    const sum = a + b;
+    return sum;
+}
+
+function multiply(a, b){
+    const product = a*b;
+    return product;
+}
+
+function divide(a, b){  
+    const quotient = a/b;
+    return quotient;
+}
+
+function subtract(a, b){
+    const ans = a - b;
+    return ans;
+}
+
+
+
+
+
+//! 2) by using params instead of query
+
+
+
+app.get("/sum/:a/:b", function(req, res) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json((sum(a, b)))
+});
+
+app.get("/multiply", function(req, res) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json(multiply(a, b))
+});
+
+app.get("/divide", function(req, res) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json(divide(a, b))
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json(subtract(a, b))
+});
+
+app.listen(3000);
---
+++ Web-Dev/Week-5/index.js
@@ -0,0 +1,41 @@
+//! Headers, Query Params and Express
+
+
+
+
+//! Headers:- contains the extra meta deta sent by the client that is of no use to the server which are called request headers, the server also response back with some headers that are called response headers
+
+//! Http headers are key value pairs between a client(browser) and a server in an http request or reponse
+// these include content type, auth information etc.
+
+//? common headers are = autorization, content-type, referrer(which url is this request coming from)
+
+
+
+
+//! suppose we are sending the information in the form the json, then we'll tell in headers that our content type is json.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! Fetch API
+
+//? 1) when we visit a website, our briwser sends an html requests but we can see in the networks tab that there are many more requests that are sent in the background which are very important, so how can we send these background requests. by using the (fetch) funtion provided by the browser.
+
+//? 2)  look index.html
