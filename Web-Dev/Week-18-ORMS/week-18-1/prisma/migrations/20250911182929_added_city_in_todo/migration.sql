231992d98e8c6e1b2173ea2216243be514cdf863
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-09-12 00:00:47 +0530

    migrated prisma

---
+++ Web-Dev/Week-18-ORMS/week-18-1/prisma/migrations/20250911182929_added_city_in_todo/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Todo_title_key" ON "public"."Todo"("title");
