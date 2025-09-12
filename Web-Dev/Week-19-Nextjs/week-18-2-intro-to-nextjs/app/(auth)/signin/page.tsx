e59e0fd3ce11708b382184c6d00f757f35b0a14a
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-09-12 18:15:08 +0530

    completed week-18-2

---
+++ Web-Dev/Week-19-Nextjs/week-18-2-intro-to-nextjs/app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+    return(
+        <div className="h-screen w-full flex items-center justify-center text-4xl">
+            SignIn page
+        </div>
+    )
+}
---
+++ Web-Dev/Week-19-Nextjs/week-18-2-intro-to-nextjs/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+    return(
+        <div className="h-screen w-full flex items-center justify-center text-4xl">
+            SignUp page
+        </div>
+    )
+}
